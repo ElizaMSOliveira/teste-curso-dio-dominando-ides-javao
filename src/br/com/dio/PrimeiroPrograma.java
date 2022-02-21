65ee484db3d899c6a698ea240d96fbc9b61ef73b
Eliza Oliveira <elizamsoliveira@gmail.com>
2022-02-21 15:40:52 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(livros);
+		
+		// TODO Auto-generated method stub
+		/*int ano = 2022;
+		int nasc = 1979;
+		
+		System.out.println("Eliza sua idade é: "+ (ano-nasc));*/
+				
+	}
+
+}
+class Livros{
+	private String nome;
+	private String npag;
+	
+}
